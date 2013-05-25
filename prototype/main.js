00825a3460efea0957cdf6be730c4f128306df16
Jeffrey Zhang <caretcaret@gmail.com>
2013-05-25 12:37:26 -0700

    Set up object models.

--- prototype/main.js
+++ prototype/main.js
@@ -1,38 +1,80 @@
 (function(){
-var graph = d3.select('#graph');
-var construction = d3.select('#construction');
+var harvest = d3.select('#harvest svg');
+var constructor = d3.select('#picker svg');
 
-/* vertex construction interface */
-function new_vertex() {
-  var x = 87.5;
-  var y = 80;
-  construction.append('circle')
-    .attr('cx', x)
-    .attr('cy', y)
-    .attr('r', 75)
-    .attr('fill', '#7fbc41')
-    .attr('stroke', '#4d9221')
-    .attr('stroke-width', 2);
-  construction.append('text')
-    .attr('x', x)
-    .attr('y', y)
-    .attr('fill', '#ffffff')
-    .attr('font-size', '144px')
-    .attr('style', 'text-anchor: middle; dominant-baseline: central;')
-    .text('+');
+/* The distinction between data and visual presentation
+ * is made through the naming of the objects:
+ **************
+ * Data components use the technical terms:
+ * Data text that the user inputs is called the "source".
+ * By using the "controls", the user encapsulates the data
+ * into "vertices" and "edges". Each vertex and edge consists
+ * of a (possibly empty) list of "attributes"; the first one
+ * is the "title" and may be empty. A "group" groups similar
+ * vertices and provides a common interface for the attributes.
+ * Names for each attribute is the "label". The whole of the
+ * processed data is the "graph".
+ **************
+ * Visual components are described metaphorically:
+ * source -> farm
+ * controls -> picker
+ * vertices -> fruits
+ * edges -> vines
+ * attributes -> tag
+ * label -> property
+ * group -> basket
+ * graph -> harvest
+ */
+
+function Graph(vertices, edges, groups) {
+  if (typeof(vertices) === undefined)
+    vertices = [];
+  if (typeof(edges) === undefined)
+    edges = [];
+  if (typeof(groups) === undefined)
+    groups = [];
+  this.vertices = vertices;
+  this.edges = edges;
+  this.groups = groups;
 }
 
-d3.select('#btn_vertex')
-  .on('click', new_vertex);
+/* TODO: integrate source data model with selections */
+function Source() {}
+function Attribute() {}
 
-/* vertex examples */
-graph.selectAll('circle')
-  .data([20, 50, 120])
-  .enter().append('circle')
-  .attr('cy', 90)
-  .attr('cx', String)
-  .attr('r', 10)
-  .attr('fill', '#7fbc41')
-  .attr('stroke', '#4d9221')
-  .attr('stroke-width', 2);
-})();
+function Vertex(attributes, color) {
+  if (typeof(attributes) === undefined)
+    attributes = [""];
+  if (typeof(color) === undefined) {
+    // TODO: set random color
+  }
+  this.attributes = attributes;
+  this.color = color;
+}
+
+function Edge(from, to, bidirectional) {
+  if (typeof(bidirectional) === undefined)
+    bidirectional = true;
+  this.from = from;
+  this.to = to;
+  this.bidirectional = bidirectional;
+}
+
+function Group(vertices, labels) {
+  if (typeof(vertices) === undefined)
+    vertices = [];
+  if (typeof(labels) === undefined)
+    labels = [];
+  this.vertices = vertices;
+  this.labels = labels;
+}
+
+/* TODO: integrate fruits with d3 force model */
+function Fruit() {}
+function Vine() {}
+function Basket() {}
+
+function showVertex(vertex) {
+  // TODO: Set picker viewer to display title/attributes of the
+  // selected vertex
+}
