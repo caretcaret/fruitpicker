5b98da8283a3341fcae9c3e7ea7433e2afa1c46d
Jeffrey Zhang <caretcaret@gmail.com>
2013-05-31 13:25:39 -0700

    Refactoring

--- prototype/main.js
+++ prototype/main.js
@@ -40,21 +40,221 @@ function random_color() {
  * graph -> harvest
  */
 
-// TODO: refactor into a structure representing state of the page
-function Graph(vertices, edges, groups, selection) {
-  if (typeof(vertices) === 'undefined')
-    vertices = [];
-  if (typeof(edges) === 'undefined')
-    edges = [];
-  if (typeof(groups) === 'undefined')
-    groups = [];
-  if (typeof(selection) === 'undefined')
-    selection = [];
-  this.vertices = vertices;
-  this.edges = edges;
-  this.groups = groups;
-  this.selection = selection;
+function State(json) {
+  if (typeof(json) === 'undefined') {
+    this.init();
+  } else {
+    // load a new state from the saved state json
+    this.load(json);
+  }
+  this.setupUI();
 }
+
+State.prototype = {
+  init: function() {
+    this.vertices = [];
+    this.edges = [];
+    this.groups = [];
+    this.selection = [];
+  },
+  loadData: function(json) {
+    try {
+      var obj = JSON.parse(json);
+      this.vertices = obj.vertices;
+      this.edges = obj.vertices;
+      this.groups = [];
+      this.selection = [];
+    } catch(e) {
+      console.log(e);
+      // fall back to loading new page
+      this.init();
+    }
+  },
+  setupUI: function() {
+    var state = this;
+    this.farm = d3.select('#farm');
+    this.harvest = d3.select('#harvest svg');
+    this.fruit_picker = d3.select('#fruit_picker');
+    this.vine_picker = d3.select('#vine_picker');
+    this.basket_picker = d3.select('#basket_picker');
+    this.fruits = harvest.selectAll('.fruit');
+    this.vines = harvest.selectAll('.vine');
+
+    this.force = d3.layout.force()
+      .size(this.getHarvestDimensions())
+      .nodes(this.vertices)
+      .links(this.edges)
+      .charge(-50)
+      .on('tick', function() {
+        state.tick();
+      });
+
+    d3.select('#btn_vertex')
+      .on('mouseup', function(e) {
+        var sel = rangy.getSelection();
+        var attr = new Attribute(sel);
+        var v = state.addVertex(attr);
+        state.selectVertex(v);
+      });
+
+    d3.select('#btn_edge')
+      .on('mouseup', function(e) {
+        var e = new Edge();
+        state.selectEdge(e);
+      });
+  },
+  start: function() {
+    var state = this;
+    this.force.start();
+    document.getElementById('farm').focus();
+    window.onresize = function() {
+      state.resize();
+    };
+  },
+  selectVertex: function(vertex) {
+    // generate strings to display in the picker
+    var toShow = vertex.attributes.map(function(attr) {
+      if (attr.text === "")
+        return "+";
+      return attr.text;
+    });
+    // deselect others from picker
+    this.vine_picker.style('display', 'none');
+    this.basket_picker.style('display', 'none');
+    this.fruit_picker.style('display', 'inherit');
+
+    // remove other fruits
+    this.fruit_picker.selectAll('button').remove();
+
+    // colors
+    var orig = vertex.color.toString();
+    var brighter = vertex.color.brighter(0.6).toString();
+    var darker = vertex.color.darker(0.6).toString();
+    var gradient = 'linear-gradient(' + brighter + ',' + orig + ')';
+
+    // deselect other vertices and select this one
+    this.selection = [vertex];
+    this.fruits.attr('stroke', function(d) {
+      if (state.selection.indexOf(d) === -1)
+        return '#ffffff';
+      return darker;
+    });
+
+    var onClickAttribute = function(d, i) {
+      // if user clicked an attribute, update it with the new selection
+      var sel = rangy.getSelection();
+      var attr = new Attribute(sel);
+      // if selection is empty, scroll source to prior selection
+      if (attr.text === "") {
+        // TODO
+      } else {
+        vertex.updateAttribute(i, attr);
+        state.selectVertex(vertex);
+      }
+    };
+
+    // add each attribute
+    this.fruit_picker.selectAll('button')
+      .data(toShow)
+      .enter().insert('button')
+      .text(function(d) { return d; })
+      .attr('style', 'background:' + gradient + '; background: -webkit-' + gradient + ';')
+      // support both vendor prefixes
+      .style('border', '1px solid ' + darker)
+      .on('mouseup', onClickAttribute);
+
+    // add 'add attribute' button
+    this.fruit_picker.insert('button')
+      .text('+')
+      .attr('style', 'background:' + gradient + '; background: -webkit-' + gradient + ';')
+      .style('border', '1px solid ' + darker)
+      .style('border-bottom-left-radius', '5px')
+      .style('border-bottom-right-radius', '5px')
+      // add new attribute on click
+      .on('mouseup', function(d) { onClickAttribute(d, -1); });
+
+    // TODO: hover styles
+  },
+  addVertex: function(title, color) {
+    var state = this;
+    if (typeof(title) === 'undefined')
+      title = new Attribute();
+    // create vertex and add it to the graph/harvest
+    var v = new Vertex([title], color);
+    this.vertices.push(v);
+    
+    this.fruits = this.fruits.data(this.vertices);
+    this.fruits.enter().insert('circle')
+      .classed('fruit', true)
+      .attr('r', 7.5)
+      .attr('fill', function(d) { return d3.hsl(d.color).toString(); })
+      .attr('stroke-width', 2)
+      .call(this.force.drag)
+      .on('mouseup', function(d) {
+        state.selectVertex(d);
+      });
+
+    this.force.start();
+
+    return v;
+  },
+  selectEdge: function(edge) {
+    // picker for edges requires 2 buttons for each vertex and
+    // a toggle button for the directionality.
+    // first, remove everything in the picker
+    this.fruit_picker.style('display', 'none');
+    this.basket_picker.style('display', 'none');
+
+    // select this edge
+    this.vine_picker.style('display', 'inherit');
+    this.selection = [edge];
+
+    // remove other edge
+    this.vine_picker.selectAll('button').remove();
+
+    // add edge picker ui
+    this.vine_picker.append('button')
+      .text(function(d) {
+        if (typeof(edge.from) === 'undefined')
+          return "+";
+        return edge.from.attributes[0].text;
+      });
+    this.vine_picker.append('button')
+      .text(function(d) {
+        if (edge.bidirectional)
+          return '\u2194'; // horizontal double arrow
+        return '\u2192'; // right arrow
+      });
+    this.vine_picker.append('button')
+      .text(function(d) {
+        if (typeof(edge.to) === 'undefined')
+          return "+";
+        return edge.to.attributes[0].text;
+      });
+  },
+  addEdge: function() {
+    var e = new Edge(from, to, bidirectional);
+    this.edges.push(e);
+    return e;
+  },
+  tick: function() {
+    this.vines.attr('x1', function(d) { return d.source.x; })
+      .attr('y1', function(d) { return d.source.y; })
+      .attr('x2', function(d) { return d.target.x; })
+      .attr('y2', function(d) { return d.target.y; });
+
+    this.fruits.attr('cx', function(d) { return d.x; })
+      .attr('cy', function(d) { return d.y; });
+  },
+  getHarvestDimensions: function() {
+    var rect = document.getElementById('harvest').getBoundingClientRect();
+    return [rect.width, rect.height];
+  },
+  resize: function() {
+    this.force.size(this.getHarvestDimensions())
+      .start();
+  }
+};
 
 /* TODO: integrate source data model with source selections */
 function Attribute(selection) {
@@ -90,7 +290,7 @@ Vertex.prototype.updateAttribute = function(index, attr) {
   else
     this.attributes.push(attr);
   return true;
-}
+};
 
 function Edge(from, to, bidirectional) {
   if (typeof(bidirectional) === 'undefined')
@@ -109,173 +309,7 @@ function Group(vertices, labels) {
   this.labels = labels;
 }
 
-function showVertex(vertex) {
-  // generate strings to display in the picker
-  var toShow = vertex.attributes.map(function(attr) {
-    if (attr.text === "")
-      return "+";
-    return attr.text;
-  });
-  // remove previous vertex from picker
-  tags.selectAll('button').remove();
-  tags.classed('fruit', true);
-  tags.classed('vine', false);
-
-  var orig = vertex.color.toString();
-  var brighter = vertex.color.brighter(0.6).toString();
-  var darker = vertex.color.darker(0.6).toString();
-  var gradient = 'linear-gradient(' + brighter + ',' + orig + ')';
-
-  // deselect other vertices and select this one
-  fruits.attr('stroke', function(d) {
-    return (d === vertex) ? darker : '#ffffff';
-  });
-
-  var onClickAttribute = function(d, i) {
-    // if user clicked an attribute, update it with the new selection
-    var sel = rangy.getSelection();
-    var attr = new Attribute(sel);
-    // if selection is empty, scroll source to prior selection
-    if (attr.text === "") {
-      // TODO
-    } else {
-      vertex.updateAttribute(i, attr);
-      showVertex(vertex);
-    }
-  }
-  
-  // add each attribute
-  tags.selectAll('button')
-    .data(toShow)
-    .enter().insert('button')
-    .text(function(d) { return d; })
-    .attr('style', 'background:' + gradient + '; background: -webkit-' + gradient + ';')
-    // support both vendor prefixes
-    .style('border', '1px solid ' + darker)
-    .on('mouseup', onClickAttribute);
-
-  // add 'add attribute' button
-  tags.insert('button')
-    .text('+')
-    .attr('style', 'background:' + gradient + '; background: -webkit-' + gradient + ';')
-    // support both vendor prefixes
-    .style('border', '1px solid ' + darker)
-    .style('border-bottom-left-radius', '5px')
-    .style('border-bottom-right-radius', '5px')
-    // add new attribute on click
-    .on('mouseup', function(d) { onClickAttribute(d, -1)});
-
-  // TODO: hover styles
-}
-
-function showEdge(edge) {
-  // picker for edges requires 2 buttons for each vertex and
-  // a toggle button for the directionality.
-  // first, remove anything shown in the picker
-
-  tags.selectAll('button').remove();
-  tags.classed('fruit', false);
-  tags.classed('vine', true);
-
-  tags.append('button')
-    .text(function(d) {
-      if (typeof(edge.from) === 'undefined')
-        return '+';
-      return edge.from.attributes[0].text;
-    })
-  tags.append('button')
-    .text(function(d) {
-      if (edge.bidirectional)
-        return '\u2194'; // horizontal double arrow
-      return '\u2192'; // right arrow
-     })
-  tags.append('button')
-    .text(function(d) {
-      if (typeof(edge.to) === 'undefined')
-        return '+';
-      return edge.from.attributes[0].text;
-    });
-}
-
-function addEdge(from, to, bidirectional) {
-  var e = new Edge(from, to, bidirectional);
-}
-
-var G = new Graph();
-
-var force = d3.layout.force()
-  .size([harvest_w, harvest_h])
-  .nodes(G.vertices)
-  .links(G.edges)
-  .charge(-50)
-  .on('tick', tick);
-
-var fruits = harvest.selectAll('.fruit');
-var vines = harvest.selectAll('.vine');
-
-force.start();
-
-function tick() {
-  vines.attr('x1', function(d) { return d.source.x; })
-    .attr('y1', function(d) { return d.source.y; })
-    .attr('x2', function(d) { return d.target.x; })
-    .attr('y2', function(d) { return d.target.y; });
-
-  fruits.attr('cx', function(d) { return d.x; })
-    .attr('cy', function(d) { return d.y; });
-}
-
-// TODO
-function deleteVertex(vertex) {}
-
-// when user clicks the vertex button
-function addVertex(title, color) {
-  if (typeof(title) === 'undefined')
-    title = new Attribute();
-  // create vertex and add it to the graph/harvest
-  var v = new Vertex([title], color);
-  force.nodes().push(v);
-  fruits = fruits.data(force.nodes());
-
-  // add the corresponding circle
-  fruits.enter().insert('circle')
-    .attr('class', 'fruit')
-    .attr('r', 7.5)
-    .attr('fill', function(d) { return d3.hsl(d.color).toString(); })
-    .attr('stroke', '#ffffff')
-    .attr('stroke-width', 2)
-    .call(force.drag)
-    .on('mouseup', showVertex);
-
-  force.start();
-
-  return v;
-}
-
-d3.select('#btn_vertex')
-  .on('mouseup', function(e) {
-    var sel = rangy.getSelection();
-    var attr = new Attribute(sel);
-    var v = addVertex(attr);
-    showVertex(v);
-  });
-
-d3.select('#btn_edge')
-  .on('mouseup', function(e) {
-    var e = new Edge();
-    showEdge(e);
-  })
-
-// set the caret inside the farm on page load
-document.getElementById('farm').focus();
-
-// on resize, recalculate dimensions of the harvest
-window.onresize = function() {
-  harvest_rect = d3.select('#harvest')[0][0].getBoundingClientRect();
-  var harvest_w = harvest_rect.width;
-  var harvest_h = harvest_rect.height;
-  force.size([harvest_w, harvest_h])
-    .start();
-};
+var state = new State();
+state.start();
 
 })();
